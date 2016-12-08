2ecdcc6af0aab1f21288c0526d8af09a0e488808
Administrator <Administrator@PC-20150829IFXB>
2016-12-08 15:38:35 +0800

    补充1季1课3题说明

--- freedom-leader/src/main/java/lesson1/question3/Application.java
+++ freedom-leader/src/main/java/lesson1/question3/Application.java
@@ -10,15 +10,17 @@ public class Application {
 		byte[][] array = new byte[rows][cols];
 		
 		long rowStart = System.nanoTime();
-		rowFirst(array, rows, cols);
+		long rowCount = rowFirst(array, rows, cols);
 		long rowEnd = System.nanoTime();
 		
 		long colStart = System.nanoTime();
-		colFirst(array, rows, cols);
+ 		long colCount = colFirst(array, rows, cols);
 		long colEnd = System.nanoTime();
 		
 		long rowCost = rowEnd - rowStart;
 		long colCost = colEnd - colStart;
+		System.out.println(String.format("行优先访问二维数组元素总数 : %d",rowCount));
+		System.out.println(String.format("列优先访问二维数组元素总数 : %d",colCount));
 		System.out.println(String.format("行优先访问时间全部元素耗时 : %d nano", rowCost));
 		System.out.println(String.format("列优先访问时间全部元素耗时 : %d nano", colCost));
 		System.out.println(String.format("行优先是列优先的%d倍", colCost / rowCost));
@@ -28,28 +30,34 @@ public class Application {
 	/**
 	 * 行优先
 	 * */
-	public static final void rowFirst(byte[][] array,int rows,int cols)
+	public static final long rowFirst(byte[][] array,int rows,int cols)
 	{
+		long count = 0;
 		for (int i = 0; i < rows; i++) 
 		{
 			for (int j = 0; j < cols; j++)
 			{
 				byte v = array[i][j];
+				count++;
 			}
 		}
+		return count;
 	}
 	
 	/**
 	 * 列优先
 	 * */
-	public static final void colFirst(byte[][] array,int rows,int cols)
+	public static final long colFirst(byte[][] array,int rows,int cols)
 	{
-		for (int i = 0; i < cols; i++) 
+		long count = 0;
+		for (int j= 0; j < cols; j++) 
 		{
-			for (int j = 0; j < rows; j++)
+			for (int i = 0; i < rows; i++)
 			{
-				byte v = array[j][i];
+				byte v = array[i][j];
+				count++;
 			}
 		}
+		return count;
 	}
 }
